b2c5a44f6cdf074eb411cf9c1322a9943eaa788b
David Hillock <dhillock@rogers.com>
2020-02-10 08:20:50 -0500

    Feat: Added Gulp Script

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,16 @@ const terser = require("gulp-terser"),
   rename = require("gulp-rename"),
   sass = require("gulp-sass"),
   cssnano = require("gulp-cssnano"),
-  autoprefixer = require("gulp-autoprefixer");
+  autoprefixer = require("gulp-autoprefixer"),
+  browserSync = require("browser-sync");
 
 gulp.task("scripts", function(done) {
   return gulp
     .src("./js/*.js") //input files fore gulp to minify
     .pipe(terser()) // calls the terser functrion on these files
     .pipe(rename({ extname: ".min.js" })) // rename minified file
-    .pipe(gulp.dest("./build/js")); // creates a build folder and places min files in trhere
+    .pipe(gulp.dest("./build/js")) // creates a build folder and places min files in trhere
+    .pipe(browserSync.stream());
 });
 
 //This is for SASS, check your file-paths
@@ -32,7 +34,19 @@ gulp.task("sass", function() {
       .pipe(rename("style.min.css"))
       //adds our final output to build dir
       .pipe(gulp.dest("./build/css"))
-    //syncs browser whenever a change is made and saved
-    // .pipe(browseSync.stream())
+      //syncs browser whenever a change is made and saved
+      .pipe(browserSync.stream())
   );
 });
+
+gulp.task("watch", function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  gulp.watch("js/*.js", gulp.series(["scripts"]));
+  gulp.watch("./html").on("change", browserSync.reload);
+});
+
+gulp.task("default", gulp.series("watch"));
