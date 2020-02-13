8255774813502244bd1fe5581ce85545e2cdb5ba
David Hillock <dhillock@rogers.com>
2020-02-12 21:58:35 -0500

    Fix: Fixed the spinner

--- js/index.js
+++ js/index.js
@@ -1,48 +1,49 @@
 $(document).ready(function() {
-  //  $(".dropDown").change(function(selected) {
-  //
+  $(".dropDown").change(function(selected) {
+    //
 
-  let userSelected = $(".dropDown").val();
+    let userSelected = $(".dropDown").val();
 
-  const $waitingSpinner = $(".theSpinner").hide();
+    const $waitingSpinner = $(".theSpinner").show();
 
-  userSelected = "business";
+    // userSelected = "business";
 
-  // console.log(userSelected);
-  // alert("asdfasdf");
+    // console.log(userSelected);
+    // alert("asdfasdf");
 
-  $waitingSpinner.show();
+    $waitingSpinner.show();
+    $(".nycArticles").empty();
 
-  $.ajax({
-    dataType: "json",
-    method: "GET",
-    url: `https://api.nytimes.com/svc/topstories/v2/${userSelected}.json?api-key=HxZYvgzO9JlxK5V2CK7GKd7i5WsBPHnI`
-  })
+    $.ajax({
+      dataType: "json",
+      method: "GET",
+      url: `https://api.nytimes.com/svc/topstories/v2/${userSelected}.json?api-key=HxZYvgzO9JlxK5V2CK7GKd7i5WsBPHnI`
+    })
 
-    .done(function(sectionData) {
-      //
-      console.log(sectionData); /* Show the while array */
+      .done(function(sectionData) {
+        //
+        console.log(sectionData); /* Show the while array */
 
-      // jQuery array loop
-      $.each(sectionData.results, function(index, value) {
-        if (index > 11) return false; // Want 12 news article
+        // jQuery array loop
+        $.each(sectionData.results, function(index, value) {
+          if (index > 11) return false; // Want 12 news article
 
-        let nycImageUrl = value.multimedia[0].url;
-        let nycAbstract = value.abstract;
-        let nycArticleUrl = value.short_url;
-        // console.log(value);
-        // alert(nycImageUrl);
+          let nycImageUrl = value.multimedia[0].url;
+          let nycAbstract = value.abstract;
+          let nycArticleUrl = value.short_url;
+          // console.log(value);
+          // alert(nycImageUrl);
 
-        // <a href="${nycImageUrl}" > src ="./images/logo_w3s.gif" width="100 height="100 </a>
-        // console.log(nycAbstract, nycImage);
-        // console.log(nycImageUrl);
-        // console.log(nycAbstract);
-        // console.log(nycArticleUrl);
-        // console.log(`${index}: ${value.title}`);
+          // <a href="${nycImageUrl}" > src ="./images/logo_w3s.gif" width="100 height="100 </a>
+          // console.log(nycAbstract, nycImage);
+          // console.log(nycImageUrl);
+          // console.log(nycAbstract);
+          // console.log(nycArticleUrl);
+          // console.log(`${index}: ${value.title}`);
 
-        $(".nycArticles").append(
-          //
-          `
+          $(".nycArticles").append(
+            //
+            `
             <figure class = "figCell" > 
 
             <a href="${nycArticleUrl}" target="new">  <img src ="${nycImageUrl}" >  
@@ -54,38 +55,26 @@ $(document).ready(function() {
 
             </figure>
             `
-          //
-        );
+            //
+          );
+        }); // end of the html build, for the 12 articles
+        $waitingSpinner.hide();
+      }) // end of the .done function
+
+      .fail(function() {
+        //
+        $waitingSpinner.hide();
+
+        alert("dhFail");
+      })
+
+      .always(function() {
+        // console.log("Always...the end");
+
+        $waitingSpinner.hide();
+        // alert($(document).outerHeight());
+        // setTimeout(function(), 1000);
       });
-      //
-
-      //
-    })
-
-    .fail(function() {
-      $waitingSpinner.hide();
-
-      alert("dhFail");
-      // if (
-      //   userSelected === "--top stories--" ||
-      //   "--sections--" ||
-      //   "Sections..."
-      // ) {
-      //   event.preventDefault();
-      //   return;
-      // } else {
-      //   alert("failure to load.  Please try again.");
-      //   location.reload();
-      // }
-    })
-
-    .always(function() {
-      // console.log("Always...the end");
-
-      $waitingSpinner.hide();
-      // alert($(document).outerHeight());
-      // setTimeout(function(), 1000);
-    });
-  // }); // end of dropDown listener
+  }); // end of dropDown listener
   //
 }); // end of initial ready function
