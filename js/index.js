746dba90351c563b7ef196d7216cb8cbd581a51e
David Hillock <dhMacPro61@Davids-Mac-Pro-114.local>
2020-02-12 20:52:17 -0500

    Fix: Getting NYT folder working from home.

---
+++ js/index.js
@@ -0,0 +1,89 @@
+$(document).ready(function() {
+  //  $(".dropDown").change(function(selected) {
+  //
+
+  let userSelected = $(".dropDown").val();
+
+  const $waitingSpinner = $(".theSpinner").show();
+
+  userSelected = "business";
+
+  // console.log(userSelected);
+  // alert("asdfasdf");
+
+  $waitingSpinner.show();
+
+  $.ajax({
+    dataType: "json",
+    method: "GET",
+    url: `https://api.nytimes.com/svc/topstories/v2/${userSelected}.json?api-key=HxZYvgzO9JlxK5V2CK7GKd7i5WsBPHnI`
+  })
+
+    .done(function(sectionData) {
+      //
+      console.log(sectionData); /* Show the while array */
+
+      // jQuery array loop
+      $.each(sectionData.results, function(index, value) {
+        if (index > 11) return false; // Want 12 news article
+
+        let nycImageUrl = value.multimedia[0].url;
+        let nycAbstract = value.abstract;
+        let nycArticleUrl = value.short_url;
+        // console.log(value);
+        // alert(nycImageUrl);
+
+        // <a href="${nycImageUrl}" > src ="./images/logo_w3s.gif" width="100 height="100 </a>
+        // console.log(nycAbstract, nycImage);
+        // console.log(nycImageUrl);
+        // console.log(nycAbstract);
+        // console.log(nycArticleUrl);
+        // console.log(`${index}: ${value.title}`);
+
+        $(".nycArticles").append(
+          //
+          `
+            <figure class = "figCell" > 
+
+            <a href="${nycArticleUrl}" target="new">  <img src ="${nycImageUrl}" >  
+
+                <p class="abstract"> ${nycAbstract} 
+                </p>
+            
+            </a> 
+
+            </figure>
+            `
+          //
+        );
+      }); // end of the html build, for the 12 articles
+      $waitingSpinner.hide();
+    }) // end of the .done function
+
+    .fail(function() {
+      $waitingSpinner.hide();
+
+      alert("dhFail");
+      // if (
+      //   userSelected === "--top stories--" ||
+      //   "--sections--" ||
+      //   "Sections..."
+      // ) {
+      //   event.preventDefault();
+      //   return;
+      // } else {
+      //   alert("failure to load.  Please try again.");
+      //   location.reload();
+      // }
+    })
+
+    .always(function() {
+      // console.log("Always...the end");
+
+      $waitingSpinner.hide();
+      // alert($(document).outerHeight());
+      // setTimeout(function(), 1000);
+    });
+  // }); // end of dropDown listener
+  //
+}); // end of initial ready function
